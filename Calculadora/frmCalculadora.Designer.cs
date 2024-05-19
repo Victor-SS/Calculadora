4cd257d48cdf44119d43e15a9d1843e47c61a3e4
Medi <Medi@DESKTOP-NVLNLPU>
2024-05-19 00:09:56 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/frmCalculadora.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Calculadora
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculadora));
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.cboOperacoes = new System.Windows.Forms.ComboBox();
+            this.lblValor2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblOperacoes = new System.Windows.Forms.Label();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(12, 214);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(89, 50);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lblValor1
+            // 
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor1.Location = new System.Drawing.Point(12, 38);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(80, 25);
+            this.lblValor1.TabIndex = 1;
+            this.lblValor1.Text = "Valor 1";
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor1.Location = new System.Drawing.Point(142, 37);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(121, 26);
+            this.txtValor1.TabIndex = 0;
+            this.txtValor1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValor1_KeyPress);
+            // 
+            // cboOperacoes
+            // 
+            this.cboOperacoes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboOperacoes.FormattingEnabled = true;
+            this.cboOperacoes.Location = new System.Drawing.Point(142, 122);
+            this.cboOperacoes.Name = "cboOperacoes";
+            this.cboOperacoes.Size = new System.Drawing.Size(121, 21);
+            this.cboOperacoes.TabIndex = 2;
+            // 
+            // lblValor2
+            // 
+            this.lblValor2.AutoSize = true;
+            this.lblValor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor2.Location = new System.Drawing.Point(12, 81);
+            this.lblValor2.Name = "lblValor2";
+            this.lblValor2.Size = new System.Drawing.Size(80, 25);
+            this.lblValor2.TabIndex = 4;
+            this.lblValor2.Text = "Valor 2";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 159);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(109, 25);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // lblOperacoes
+            // 
+            this.lblOperacoes.AutoSize = true;
+            this.lblOperacoes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperacoes.Location = new System.Drawing.Point(12, 116);
+            this.lblOperacoes.Name = "lblOperacoes";
+            this.lblOperacoes.Size = new System.Drawing.Size(117, 25);
+            this.lblOperacoes.TabIndex = 5;
+            this.lblOperacoes.Text = "Operações";
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor2.Location = new System.Drawing.Point(142, 81);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(121, 26);
+            this.txtValor2.TabIndex = 1;
+            this.txtValor2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValor2_KeyPress);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(142, 160);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(121, 26);
+            this.txtResultado.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(174, 214);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(89, 50);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 288);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblOperacoes);
+            this.Controls.Add(this.lblValor2);
+            this.Controls.Add(this.cboOperacoes);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.lblValor1);
+            this.Controls.Add(this.btnLimpar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCalculadora";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.frmCalculadora_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lblValor1;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.ComboBox cboOperacoes;
+        private System.Windows.Forms.Label lblValor2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblOperacoes;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Calculadora/frmCalculadora.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCalculadora_Load(object sender, EventArgs e)
+        {
+            cboOperacoes.Items.Add("Soma");
+            cboOperacoes.Items.Add("Divisão");
+            cboOperacoes.Items.Add("Subtração");
+            cboOperacoes.Items.Add("Multiplicação");
+            cboOperacoes.SelectedIndex = -1;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtValor1.Clear();
+            txtValor2.Clear();
+            cboOperacoes.SelectedIndex = -1;
+            txtResultado.Clear();
+            txtValor1.Focus();
+        }
+
+        private decimal Somar(decimal v1, decimal v2)
+        {
+            decimal result = 0;
+
+            result = v1 + v2;
+
+            return result;
+        }
+        private decimal Dividir(decimal v1, decimal v2)
+        {
+            decimal result = 0;
+
+            result = v1 / v2;
+
+            return result;
+        }
+        private decimal Subtrair(decimal v1, decimal v2)
+        {
+            decimal result = 0;
+
+            result = v1 - v2;
+
+            return result;
+        }
+        private decimal Multiplicar(decimal v1, decimal v2)
+        {
+            decimal result = 0;
+
+            result = v1 * v2;
+
+            return result;
+        }
+
+        private string ValidarPreenchimento()
+        {
+            string msgErro = string.Empty;
+
+            if(txtValor1.Text == string.Empty)
+            {
+                msgErro += "Campo VALOR 1 em Branco!\n";
+            }
+            if(txtValor2.Text == string.Empty)
+            {
+                msgErro += "Campo VALOR 2 em Branco!\n";
+            }
+            if(cboOperacoes.SelectedIndex == -1)
+            {
+                msgErro += "Campo OPERAÇÕES em Branco!\n";
+            }
+
+            return msgErro;
+        }
+
+        private void txtValor1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != ',' &&
+                !char.IsControl(e.KeyChar) ||
+                e.KeyChar == ',' && txtValor1.Text.Contains(','))
+            {
+                e.Handled = true;
+            }
+            return;
+        }
+        private void txtValor2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != ',' &&
+               !char.IsControl(e.KeyChar) ||
+               e.KeyChar == ',' && txtValor2.Text.Contains(','))
+            {
+                e.Handled = true;
+            }
+            return;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            decimal valor1 = 0;
+            decimal valor2 = 0;
+            decimal resultado = 0;
+            string mensagem = ValidarPreenchimento();
+
+            if (mensagem != string.Empty)
+            {
+                MessageBox.Show(mensagem, "Campo Inválido!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            valor1 = Convert.ToDecimal(txtValor1.Text);
+            valor2 = Convert.ToDecimal(txtValor2.Text);
+
+
+            switch (cboOperacoes.SelectedItem.ToString())
+            {
+                case "Soma":
+                    resultado = Somar(valor1, valor2);
+                    break;
+                case "Divisão":
+                    if(valor1 == 0 || valor2 == 0)
+                    {
+                        MessageBox.Show("Divisão por zero não é permitida!",
+                            "Erro na Divisão", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    resultado = Dividir(valor1, valor2);
+                    break;
+                case "Subtração":
+                    resultado = Subtrair(valor1, valor2);
+                    break;
+                case "Multiplicação":
+                    resultado = Multiplicar(valor1, valor2);
+                    break;
+            }
+            txtResultado.Text = resultado.ToString();
+        }
+    }
+}
